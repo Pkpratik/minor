7db297ec8a20240c5dd6323e3ae947a0d786fb8b
pratik agrawal <46810588+Pkpratik@users.noreply.github.com>
2021-01-28 19:05:38 +0530

    removed warnings

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import LoginButton from "./components/LoginButton"
 import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
 import "./App.css"
 
 //import Search from "./components/Search";
@@ -25,7 +24,7 @@ class App extends Component {
       <ApolloProvider client={client}>
         <div id="main">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Readers Corner - The Books Store</a>
+        <p class="navbar-brand" >Readers Corner - The Books Store</p>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -33,7 +32,6 @@ class App extends Component {
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-            <Profile/>
                 <li class="nav-item mr-2">
                   <LoginButton />
                     
