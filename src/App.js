eb41fd3dab02877925f26be9f0ba9ceb38796995
pratik agrawal <46810588+Pkpratik@users.noreply.github.com>
2021-01-28 20:46:34 +0530

    added login logout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import LoginButton from "./components/LoginButton"
 import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 import "./App.css"
 
 //import Search from "./components/Search";
@@ -23,20 +24,21 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <div id="main">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <p class="navbar-brand" >Readers Corner - The Books Store</p>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <p className="navbar-brand" >Readers Corner - The Books Store</p>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item mr-2">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+                <Profile/>
+                <li className="nav-item mr-2">
                   <LoginButton />
                     
                 </li>
-                <li class="nav-item">
+                <li className="nav-item">
                     <LogoutButton/>
                     
                 </li>
---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {useAuth0} from '@auth0/auth0-react'
+
+export default function Profile() {
+    const {user,isAuthenticated} = useAuth0();
+    
+    return (
+        isAuthenticated && (<>
+            
+            {console.log("working profile",JSON.stringify(user,null,2))}
+        
+            <li class="nav-item">
+                    <p className="nav-link" ><img src={user.picture} class="rounded-circle" id="profile_img"
+                            alt="profile" /></p>
+                </li>
+                <li class="nav-item" id="user_name">
+                    <p className="nav-link" ><span class="font-weight-bolder">Hello</span> {user.name}
+                    </p>
+                </li>
+        
+        </>
+
+
+        ))
+}
